13052487a52320127ac6712e33fdb51ba291e638
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-08-17 12:33:38 +0800

    1

--- clean_architecture/lib/main.dart
+++ clean_architecture/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'app/app.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  //WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
-
